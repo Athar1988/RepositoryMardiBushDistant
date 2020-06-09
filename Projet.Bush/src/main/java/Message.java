f6e0837aa988554565aab0bf0f7d4ea0526dc9d7
Athar1988 <hakim.athar.88@gmail.com>
2020-06-09 15:35:45 +0100

    supprimer les messages sauf boucle monBranche

--- Projet.Bush/src/main/java/Message.java
+++ Projet.Bush/src/main/java/Message.java
@@ -3,10 +3,9 @@ public class Message {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("eeeee");
-System.out.println("ssssssssss");
+
 for(int i=0; i<10; i++) {
-	System.out.println("ssssssssss");
+	System.out.println("ooooooooo");
 }
 	}
 
