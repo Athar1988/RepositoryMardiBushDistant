c7b5f0e93f8812a22dd13c837082b59c2bde9dbd
Athar1988 <hakim.athar.88@gmail.com>
2020-06-09 10:51:06 +0100

    ajouter une boucle et une classe

--- Projet.Bush/src/main/java/Message.java
+++ Projet.Bush/src/main/java/Message.java
@@ -4,6 +4,13 @@ public class Message {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("eeeee");
+System.out.println("eeeee");
+System.out.println("eeeee");
+System.out.println("eeeee");
+for (int i=0; i<10; i++) {
+	System.out.println("eeeee");
+
+}
 	}
 
 }
